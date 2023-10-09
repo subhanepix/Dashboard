72fc176b2fa77341de17adf54b7045f6d36e9f6d
subhanepix <94199944+subhanepix@users.noreply.github.com>
2023-10-08 23:26:48 -0500

    removed random space

--- Cprit_Impact_Summary.py
+++ Cprit_Impact_Summary.py
@@ -6,12 +6,6 @@ st.set_page_config(layout="wide")
 with open('style.css') as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
-
-
-
-
-
-
 cprit_grants_funded_df = pd.read_excel('datasets/CpritGrantsFunded.xlsx')
 cgf_copy = cprit_grants_funded_df.copy()
 
